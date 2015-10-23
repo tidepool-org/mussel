7d17f3024cb0ac95e73d4bd4408d57746531bc81
nosh <nosh@alum.mit.edu>
2015-10-23 14:31:59 -0400

    added express service to get auths and deauth shims - so can be used from external apps

--- musselrunner.js
+++ musselrunner.js
@@ -1,5 +1,36 @@
 var Mussel = require('./index.js');
+var express = require("express");
+var app = express();
 
+app.get("/auths/:userid", function(req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	var auths = mussel.getAuths(req.params.userid, function(err, response) {
+	if (err) {
+		console.log('error:'+err);
+		res.send(error);
+	} else {
+		console.log(response);
+		res.send(response);
+	}
+	});
+});
+
+app.get("/deauthorize/:shim", function(req, res) {
+	var username = req.query.username;
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	mussel.deauthorize(req.params.shim, username, function(err, response) {
+		if (err) {
+			console.log('error:'+err);
+			res.send(err);
+		} else{
+			console.log(response);
+			res.send(response);
+		}
+	});
+
+});
 
 var SHIMMER_HOST=process.env.SHIMMER_HOST
 
@@ -57,10 +88,7 @@ function syncActivities() {
 }
 
 
-setTimeout(function() {
-	console.log('exiting');
-	process.exit();
-}, 30000);
+
 
 function done(err, response) {
 	if (err != null) {
@@ -79,22 +107,38 @@ var mussel = new Mussel(shimmerConfig, tidepoolConfig);
 switch(process.argv[2]) {
 	case 'sync':
 		console.log('Syncing activities');
+		exitAfter(30000);
 		syncActivities();
 		break;
 	case 'delete':
 		console.log('Deleting activity notes');
+		exitAfter(30000);
 		mussel.deleteActivityNotes(process.argv[3], done);
 		//0e5fab3f1a
 		break; 
+	case 'service':
+		var port = process.env.PORT || 5000;
+		 app.listen(port, function() {
+		   console.log("Listening on " + port);
+		 });
+		 break;
 	default:
 		console.log('Incorrect arguments');
 		console.log('Usage:');
 		console.log('node musselrunner.js sync');
 		console.log('or');
 		console.log('node musselrunner.js delete userid');
+		console.log('or');
+		console.log('node musselrunner.js service');
 
 }
 
+function exitAfter(millis) {
+	setTimeout(function() {
+				console.log('exiting');
+				process.exit();
+			}, millis);
+}
 
 
 
